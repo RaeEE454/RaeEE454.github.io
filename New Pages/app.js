801a68e5dad9ba97e866f230eb138efc75fab7cf
RaeE454 <rwatkins535@gmail.com>
2023-03-27 19:16:27 -0600

    moved files from portfolio prjt to github acct

---
+++ New Pages/app.js
@@ -0,0 +1,10 @@
+
+function linkedInBtn(link){
+    location.href = "https://www.linkedin.com/in/raewatkins53535/";
+};
+
+// // Rating Initialization
+// $(document).ready(function() {
+//     $('#rateMe1').mdbRate();
+//   });
+
